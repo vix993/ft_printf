fdf86d45373aa415c9c07756a60de3c8f439231d
Victor <victor.as.nascimento@gmail.com>
2020-12-14 15:12:23 -0300

    ft_printf_test fully passing

--- includes/main.c
+++ includes/main.c
@@ -5,4 +5,6 @@ int	main()
 {
 	printf("expected: %%\n%0133.70%\n%086.113%\n%-188.46%\n");
 	ft_printf("actual: %%\n%0133.70%\n%086.113%\n%-188.46%\n");
+	printf("expected: %*s\n", -3, "hello");
+	ft_printf("actual: %*s\n", -3, "hello");
 }
--- src/other.c
+++ src/other.c
@@ -3,12 +3,12 @@
 
 static t_top		*display_percent(t_top *top)
 {
-	if (top->convert[3] == '0' && top->precision <= 0 && !top->convert[0])
+	if (top->convert[3] == '0' && !top->convert[0])
 	{
 		top->precision = top->fwidth;
 		top->fwidth = 0;
 	}
-	if (top->convert[0] != '-')
+	if (top->convert[0] != '-' && top->convert[3] != '0')
 		display_gap(top, ' ', top->fwidth - 1, 1);
 	if (top->convert[3] == '0' && top->convert[0] != '-')
 		display_gap(top, '0', top->precision - 1, 1);
@@ -33,7 +33,7 @@ t_top			*display_other(t_top *top)
 	}
 	if (top->convert[0] != '-')
 		display_gap(top, ' ', top->fwidth - 1, 1);
-	if (top->convert[3] == '0')
+	if (top->convert[3] == '0' && top->convert[0] != '-')
 		display_gap(top, '0', top->precision - 1, 1);
 	write(1, &top->f_copy[top->i], 1);
 	if (top->convert[0] == '-')
