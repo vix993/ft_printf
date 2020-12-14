33a9304f407f1d4d4af37c663d77e1b263642bd4
Victor Nascimento <vnascime@c1r1p4.42sp.org.br>
2020-12-14 17:43:37 -0300

    norminette final

--- src/libft/ft_strcmp.c
+++ src/libft/ft_strcmp.c
@@ -1,10 +1,21 @@
- #include "libft.h"
- #include <stddef.h>
- 
- int             ft_strcmp(const char *s1, const char *s2)
- {
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vnascime <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/14 17:23:32 by vnascime          #+#    #+#             */
+/*   Updated: 2020/12/14 17:29:45 by vnascime         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stddef.h>
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
 	if (ft_strlen(s1) > ft_strlen(s2))
 		return (ft_strncmp(s1, s2, (ft_strlen(s1))));
 	return (ft_strncmp(s1, s2, (ft_strlen(s2))));
 }
-
